8a272ffc68df2c1e6719273c7e58212eec7830d6
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-09-22 23:34:34 -0300

    feat: greeting configuration has been scoped. Now, the configuration can be changed in runtime

--- greeting-service/src/main/java/br/com/gustavo/controllers/GreetingController.java
+++ greeting-service/src/main/java/br/com/gustavo/controllers/GreetingController.java
@@ -1,6 +1,9 @@
 package br.com.gustavo.controllers;
 
+import br.com.gustavo.configuration.GreetingConfiguration;
 import br.com.gustavo.model.Greeting;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +15,16 @@ public class GreetingController {
 
     private static final String template = "Hello, %s";
     private final AtomicLong counter = new AtomicLong();
+    
+    @Autowired
+    private GreetingConfiguration configuration;
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "") String name) {
+        if(name.isEmpty()) name = configuration.getDefaultValue();
+        
+        return new Greeting(
+        		counter.incrementAndGet(),
+        		String.format(template, configuration.getGreeting(), name));
     }
 }
