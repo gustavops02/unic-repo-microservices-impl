edd3efc8ae2e457dd3b8a7d95b6b1a449d0933c6
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-09-26 22:09:34 -0300

    model mapping for jpa

--- cambio-service/src/main/java/br/com/gustavo/model/Cambio.java
+++ cambio-service/src/main/java/br/com/gustavo/model/Cambio.java
@@ -3,13 +3,33 @@ package br.com.gustavo.model;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+
+@Entity
 public class Cambio {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "from_currency", nullable = false, length = 3)
 	private String from;
+	
+	@Column(name = "to_currency", nullable = false, length = 3)
 	private String to;
+	
+	@Column(nullable = false)
 	private BigDecimal conversionFactior;
+	
+	@Transient
 	private BigDecimal convertedValue;
+	
+	@Transient
 	private String environment;
 	
 	public Cambio() {
