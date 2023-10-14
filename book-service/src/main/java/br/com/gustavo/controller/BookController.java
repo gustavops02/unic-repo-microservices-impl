af565ba1bd09bc6292a5d022c3fd1e0a156e7f76
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-10-14 11:23:47 -0300

    feat: update the Controller to use Feign Client

--- book-service/src/main/java/br/com/gustavo/controller/BookController.java
+++ book-service/src/main/java/br/com/gustavo/controller/BookController.java
@@ -2,15 +2,20 @@ package br.com.gustavo.controller;
 
 
 import br.com.gustavo.model.Book;
+import br.com.gustavo.proxy.CambioProxy;
 import br.com.gustavo.repository.BookRepository;
+import br.com.gustavo.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/book-service")
@@ -22,6 +27,33 @@ public class BookController {
     @Autowired
     private BookRepository repository;
 
+    @Autowired
+    private CambioProxy cambioProxy;
+
+    /*@GetMapping("/{id}/{currency}")
+    public Book findById(@PathVariable Long id,
+                         @PathVariable String currency) {
+
+        var book = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+
+        Map<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate().getForEntity(
+                "http://localhost:8000/cambio-service/{amount}/{from}/{to}",
+                Cambio.class,
+                params);
+
+        var cambio = response.getBody();
+        var port = environment.getProperty("local.server.port");
+        book.setEnvironment(port);
+        book.setPrice(cambio.getConvertedValue());
+
+        return book;
+   }*/
     @GetMapping("/{id}/{currency}")
     public Book findById(@PathVariable Long id,
                          @PathVariable String currency) {
@@ -30,10 +62,11 @@ public class BookController {
                 .orElseThrow(() -> new RuntimeException("Book not found"));
 
 
-
+        var cambio = cambioProxy.findCambio(book.getPrice(), "USD", currency);
         var port = environment.getProperty("local.server.port");
-        book.setEnvironment(port);
+        book.setEnvironment(port + " FEIGN");
+        book.setPrice(cambio.getConvertedValue());
 
         return book;
-   }
+    }
 }
